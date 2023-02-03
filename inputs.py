f06c2231c8c27c7d6278743de4d0b8a15bba52d1
JM <115455650+Jaureo@users.noreply.github.com>
2023-02-03 19:08:50 +0100

    Create inputs.py

---
+++ inputs.py
@@ -0,0 +1,11 @@
+#Feim introduir qualsevol símbol i el transformam en un string
+input("introduir qualsevol símbol")
+
+#Feim introduir qualsevol número
+float(input("introduir qualsevol número"))
+
+#Feim introduir qualsevol número enter
+int(input("introduir qualsevol número enter"))
+
+#Asignam una variable a un input
+Variable = input("Asigna un símbol a una variable")
